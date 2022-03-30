4ffcc15be1f1a3d871f2e906429b1365fdc8c689
Mengyi Qian <mengyi.qian.95@gmail.com>
2022-03-30 16:38:01 -0400

    link - pipeline
    link - pipeline

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
   <body>
     <p>
         <a href="http://trash-can.site/" target="_blank">Trash Can</a>, 
-        <a href="#" target="_blank">Pipeline</a>, 
+        <a href="/pipeline" target="_blank">Pipeline</a>, 
         <a href="/exit" target="_blank">Exit</a>, 
         <a href="#" target="_blank">Noise</a>, 
         <a href="#" target="_blank">Cloud</a>, 
