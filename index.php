007a77a93d3e677163a4be6f678d58b680cd0184
Mengyi Qian <mengyi.qian.95@gmail.com>
2022-03-30 21:03:59 -0400

    relink
    relink

--- index.php
+++ index.php
@@ -10,11 +10,11 @@
   <body>
     <p>
         <a href="http://trash-can.site/" target="_blank">Trash Can</a>, 
-        <a href="/pipeline" target="_blank">Pipeline</a>, 
-        <a href="/exit" target="_blank">Exit</a>, 
-        <a href="/noise" target="_blank">Noise</a>, 
-        <a href="/cloud" target="_blank">Cloud</a>, 
-        <a href="/refrigerator" target="_blank">Refrigerator</a>, 
+        <a href="pipeline" target="_blank">Pipeline</a>, 
+        <a href="exit" target="_blank">Exit</a>, 
+        <a href="noise" target="_blank">Noise</a>, 
+        <a href="cloud" target="_blank">Cloud</a>, 
+        <a href="refrigerator" target="_blank">Refrigerator</a>, 
         maochurong@gmail.com
     </p>
   </body>
